0eb77a6dc12be806e55b9be5fa323aae4cf25879
joaogusmaoguedes@gmail.com <joaogusmaoguedes@gmail.com@1863e1ff-5865-b99a-5849-12de0c78cf49>
2011-05-23 15:45:41 +0000

    added menu

--- Mosaico/src/pt/up/fe/android/mosaico/MainScreen.java
+++ Mosaico/src/pt/up/fe/android/mosaico/MainScreen.java
@@ -2,6 +2,8 @@ package pt.up.fe.android.mosaico;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainScreen extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,11 @@ public class MainScreen extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
