c72d9c3c6457596ebe0fe60ecd9eb163c0c2bd78
ricardo@ctrler.eu <ricardo@ctrler.eu@1863e1ff-5865-b99a-5849-12de0c78cf49>
2011-05-31 17:36:57 +0000

    Set the coordinates by hand so I could test. Implemented IdenticaAPI. Added two buttons on popPic and changed the layout. Corrected the error that made the image sometimes not load.

--- branches/nogps/src/pt/up/fe/android/mosaico/popPic.java
+++ branches/nogps/src/pt/up/fe/android/mosaico/popPic.java
@@ -2,18 +2,21 @@ package pt.up.fe.android.mosaico;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.content.DialogInterface.OnClickListener;;
 
-class popPic extends Dialog {
-	/**
-	 * @param context
-	 */
+class popPic extends Dialog{
+
 	private Photo data;
 
 	public popPic(Context context, Photo data) {
 		super(context);
 		this.data = data;
+		this.setTitle(data.getTitle());
 	}
 
 	protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +24,10 @@ class popPic extends Dialog {
 		setContentView(R.layout.windowpic);
 		ImageView imageView = (ImageView) findViewById(R.id.fullimage);
 		imageView.setImageBitmap(data.getPhoto());
+		
 	}
 	public void enddialog() {
 		dismiss();
 	}
+
 }
