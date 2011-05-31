84cdcddfa031abe6c39d30c0541bf1ea1bf92832
joaogusmaoguedes@gmail.com <joaogusmaoguedes@gmail.com@1863e1ff-5865-b99a-5849-12de0c78cf49>
2011-05-31 21:50:11 +0000

    created handler for button

--- branches/nogps/src/pt/up/fe/android/mosaico/popPic.java
+++ branches/nogps/src/pt/up/fe/android/mosaico/popPic.java
@@ -2,12 +2,11 @@ package pt.up.fe.android.mosaico;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.content.DialogInterface.OnClickListener;;
+import android.widget.Toast;
 
 class popPic extends Dialog{
 
@@ -23,6 +22,15 @@ class popPic extends Dialog{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.windowpic);
 		ImageView imageView = (ImageView) findViewById(R.id.fullimage);
+		Button identica = (Button) findViewById(R.id.identicabutton);
+		identica.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getContext(), "Click!", Toast.LENGTH_LONG).show();
+				// Chamar método p.e identica(data.getLatitude(), data.getLongitude());				
+			}});
+		
 		imageView.setImageBitmap(data.getPhoto());
 		imageView.setOnClickListener(new View.OnClickListener() {
 
