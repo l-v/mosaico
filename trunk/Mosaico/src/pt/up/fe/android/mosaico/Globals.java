49ae8d81801bc63cdb7ae30597ee0b048fcbafd1
alexander.lazarov@gmail.com <alexander.lazarov@gmail.com@1863e1ff-5865-b99a-5849-12de0c78cf49>
2011-06-03 22:38:04 +0000

    Preferences are now stored in Globals when app starts.

--- trunk/Mosaico/src/pt/up/fe/android/mosaico/Globals.java
+++ trunk/Mosaico/src/pt/up/fe/android/mosaico/Globals.java
@@ -12,7 +12,7 @@ final class Globals {
 	public static String identicaUsername="";
 	public static String identicaPassword="";
 	public static int photos_range = 2;
-	public static int photos_number = 50;
+	public static int photos_number_to_get = 50;
 	public static boolean mBusy = false;
 	public static final String FLICKR_KEY = "941a4b4ce1de1b188639082b64d6ef5e";
 	public static final String FLICKR_SECRET = "714103cba62ba18a";
