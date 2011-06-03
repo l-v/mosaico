5057a7467a8ed7a2e2914bad1d09a72c9202bf43
joaogusmaoguedes@gmail.com <joaogusmaoguedes@gmail.com@1863e1ff-5865-b99a-5849-12de0c78cf49>
2011-06-03 17:28:06 +0000

    Added incomplete FlickrAPI with test case. The JSON part of FlickrAPI must be commented before running the test, as there seems to be some trouble running that library. To run the test: Run As->JUnit Test->Android JUnit Test Launcher

--- Mosaico/src/pt/up/fe/android/mosaico/Globals.java
+++ Mosaico/src/pt/up/fe/android/mosaico/Globals.java
@@ -12,7 +12,8 @@ final class Globals {
 	public static String identicaUsername="";
 	public static String identicaPassword="";
 	public static boolean mBusy = false;
+	public static final String FLICKR_KEY = "941a4b4ce1de1b188639082b64d6ef5e";
+	public static final String FLICKR_SECRET = "714103cba62ba18a";
 	public static final int ACTIVITY_LOCATION_SETTINGS = 1;
 	public static final int ACTIVITY_GOOGLE_MAPS = 2;
-	
 }
