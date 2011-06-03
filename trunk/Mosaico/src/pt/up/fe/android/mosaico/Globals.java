113c699eeb59aff7a4afb67c2d2e146befb3ba9d
alexander.lazarov@gmail.com <alexander.lazarov@gmail.com@1863e1ff-5865-b99a-5849-12de0c78cf49>
2011-06-03 22:16:12 +0000

    Preference menu is ready.
    it gets the data in the preferences and stores it in Globals.
    
    someone has to use it now :)
    
    sorry for not using the old one. I think you will agree this looks more like preferences.

--- trunk/Mosaico/src/pt/up/fe/android/mosaico/Globals.java
+++ trunk/Mosaico/src/pt/up/fe/android/mosaico/Globals.java
@@ -11,9 +11,12 @@ final class Globals {
 	public static final String IDENTICA_URL = "http://identi.ca/api/statuses/update.xml?source=Mosaico";
 	public static String identicaUsername="";
 	public static String identicaPassword="";
+	public static int photos_range = 2;
+	public static int photos_number = 50;
 	public static boolean mBusy = false;
 	public static final String FLICKR_KEY = "941a4b4ce1de1b188639082b64d6ef5e";
 	public static final String FLICKR_SECRET = "714103cba62ba18a";
 	public static final int ACTIVITY_LOCATION_SETTINGS = 1;
 	public static final int ACTIVITY_GOOGLE_MAPS = 2;
+	public static final int ACTIVITY_PREFERENCES = 3;
 }
